11116cc3b43454fe40be784205c5b5898e4336fe
Xander <10556343+xmrgn@users.noreply.github.com>
2021-06-05 18:40:16 -0500

    formatting

--- game_bot.py
+++ game_bot.py
@@ -132,7 +132,7 @@ def gen_path_visual(board, paths, file = 'visual.jpg'):
             for x_pos in range(board_sidelength):
                 for y_pos in range(board_sidelength):
                     if (x_pos, y_pos) != (start_x, start_y) and (x_pos, y_pos) != (end_x, end_y):
-                        axs[row, col].plot(x_pos, y_pos, 'ok')
+                        axs[row, col].plot(x_pos, y_pos, 'ob')
             
             axs[row, col].plot(start_x, start_y, 'og', markersize=15)
             axs[row, col].plot(end_x, end_y, 'Dr', markersize=10)
